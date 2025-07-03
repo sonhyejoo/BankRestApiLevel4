7e93e3a86c49def039ede38ec07c28f6667bcc73
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-03 13:21:03 -0400

    Added ErrorCode enum

---
+++ Models/DTOs/ErrorCode.cs
@@ -0,0 +1,10 @@
+﻿namespace BankRestApi.Models.DTOs;
+
+public enum ErrorCode
+{
+    NonnegativeOrInvalidAmount = 1001,
+    InsufficientFunds = 1002,
+    EmptyName = 1003,
+    DuplicateId = 1004,
+    NotFound = 1005
+}
