cf81b015c5751976b1df7f8e097a29a5a9e72333
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-24 17:35:52 -0400

    Added seedUsers data

---
+++ Models/SeedUsers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankRestApi.Models;
+
+public static class SeedUsers
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new AccountContext(
+                   serviceProvider.GetRequiredService<
+                       DbContextOptions<AccountContext>>()))
+        {
+            if (context?.Users == null)
+            {
+                throw new ArgumentNullException("Null Users table");
+            }
+
+            // Look for any movies.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Users.AddRange(
+                new User
+                {
+                    AccountName = "Admin",
+                    HashedPassword = ""                
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
